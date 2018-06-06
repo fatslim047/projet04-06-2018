d63dac7093658907891bd09ff35fb60d95aa5404
fatslim047 <39806091+fatslim047@users.noreply.github.com>
2018-06-06 15:39:35 +0200

    Add files via upload

--- RDF2.java
+++ RDF2.java
@@ -3,70 +3,98 @@ package stanford_1;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.tdb.TDBFactory;
 public class RDF2 {
 	Aspect asp;
 	static String texteURI="http://texte/";
 	static String relationURI1="http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 	static String  relationURI2="http://www.example.org/tn#";
+	static String directory = "/home/emily/workspace/Extraction_2/database"; 
+	static  String namedModelURI = "http://modele/";
 	
-public static void main(String [] args) throws IOException, InterruptedException{
+public void Createmodel() throws IOException, InterruptedException{
 	
-	CreateTriple cr=new CreateTriple();
+	
 	// Create an empty Model
-	Model model = ModelFactory.createDefaultModel();
-	 
-	// Create a Resource
-	Resource texte = model.createResource(texteURI+"/"+cr.getTexte());
-    Resource aspect= model.createResource(texteURI+"/"+cr.getAsp());
-	//Resource sentiment = model.createResource(texteURI+"/"+cr.getSentis());
-	Resource type = model.createResource(texteURI+"/"+cr.getType());
+	 Dataset dataset = TDBFactory.createDataset(directory) ;
+	Model model = dataset.getNamedModel(namedModelURI);
 	
-	// Can also create properties directly . . .
+	Resource texte = null;
+	Resource aspect=null;
+	Resource sentiment=null;
+	Resource type=null;
+	// Create a Resource
+	CreateTriple cr=new CreateTriple();
+	for(int i=0;i<cr.getAsp().length;i++){
+	 texte = model.createResource(texteURI+"/"+(cr.getTexte())[i]);
+     aspect= model.createResource(texteURI+"/"+(cr.getAsp())[i]);
+	 sentiment = model.createResource(texteURI+"/"+(cr.getSentis())[i]);
+	 type = model.createResource(texteURI+"/"+cr.getType());
+	 
 	Property has= model.createProperty(relationURI1 , "has");
 	Property prop = model.createProperty(relationURI2 , "prop");	
+	
+	
+	// Can also create properties directly . . .
+	
 	//statement
-	/*Statement stmt1 = model.createStatement(texte ,  has   , type);
+	Statement stmt1 = model.createStatement(texte ,  has   , type);
 	Statement stmt2 = model.createStatement(texte  , has   , aspect);
 	Statement stmt3 = model.createStatement(aspect  , prop , sentiment);
 	
 	//add statement
 	model.add(stmt1);
 	model.add(stmt2);
-	model.add(stmt3);*/
+	model.add(stmt3);
 	//add property
-	texte.addProperty(has , type);
-	texte.addProperty(has, aspect);
-	//aspect.addProperty(prop, sentiment);
-
+	model.setNsPrefix("relation", relationURI2);
 	
-	 StmtIterator iter = model.listStatements();
-
-    // affiche l'objet, le prédicat et le sujet de chaque déclaration
-    while (iter.hasNext()) {
-        Statement stmt      = iter.nextStatement();  // obtenir la prochaine déclaration
-        Resource  subject   = stmt.getSubject();     // obtenir le sujet
-        Property  predicate = stmt.getPredicate();   // obtenir le prédicat
-        RDFNode   object    = stmt.getObject();      // obtenir l'objet
-
-        System.out.print(subject.toString()+ "  "+"\n");
-        System.out.print("  " +  predicate.toString()  + "  ");
-        if (object instanceof Resource) {
-           System.out.print(object.toString());
-        } else {
-            // l'objet est un littéral
-            System.out.println("  " + object.toString()  +  "  ");
-        }
-
-			            
 	}
-model.write(new FileWriter("trial.rdf"));
-// model.write(new FileWriter("trial.rdf"), "RDF/XML-ABBREV");
+	  model.write(new FileWriter("trial.rdf"));
+	   model.write(new FileWriter("trial.rdf"), "RDF/XML-ABBREV");
+	   System.gc();
+	  
+	model.close();
+	dataset.close();
 }
+
+	private void testQuerymodele(){
+		 Dataset dataset = TDBFactory.createDataset(directory) ;
+			Model model = dataset.getNamedModel(namedModelURI);
+			
+			Property has= model.createProperty(relationURI1 , "has");
+			Property prop = model.createProperty(relationURI2 , "prop");	
+			
+	 ResIterator iter = model.listSubjectsWithProperty(has);
+
+    while (iter.hasNext()) {
+       Resource rsc = iter.nextResource();  // obtenir la prochaine déclaration
+       System.out.println(rsc.getURI());
+        }
+		model.close();	            
+	}
+
+
+	public static void main (String[] args) throws IOException, InterruptedException{
+		RDF2 triple= new RDF2();
+		triple.Createmodel();
+		triple.testQuerymodele();
+
+	}
 }
