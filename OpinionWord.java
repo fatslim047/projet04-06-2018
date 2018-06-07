9039e708a03573ea386a3233953716019322d43d
fatslim047 <39806091+fatslim047@users.noreply.github.com>
2018-06-07 15:51:50 +0200

    Update OpinionWord.java

--- OpinionWord.java
+++ OpinionWord.java
@@ -1,24 +1,23 @@
+package stanford_1;
+
 import java.io.IOException;
 
 public class OpinionWord {
 public String Aspect ; 	
-public String Oword ;
+public String word ;
 public boolean  negation ;
 public double value ; 
 public String pos ;
+public String sentiment;
 
 
-	public OpinionWord( String Aspect , String Oword, String pos, boolean negation) throws IOException {
-	SentiWordNetDemoCode senti = new SentiWordNetDemoCode("C:/Users/Helmi/eclipse-workspace/stanford/sentiwordnet.txt");
-	this.Oword = Oword;
+	public OpinionWord( String Aspect , String word, String pos, boolean negation) throws IOException {
+	SentiWordNetDemoCode senti = new SentiWordNetDemoCode("/home/emily/workspace/Extraction_2/sentiword.txt");
+	this.word = word;
 	this.negation = negation;
 	this.Aspect= Aspect; 
 	this.pos = pos;
-	if (negation == false) {
-		value = senti.extract(Oword, pos);
-	}
-	else
-		value=-(senti.extract(Oword,pos));
+	value=senti.extract(word, pos);
 	if (pos=="v" && value==0 )
 	{
 		pos="a";
@@ -28,19 +27,34 @@ public String pos ;
 		else
 		value = senti.extract(Oword,pos);
 	}
+	
+	if(value<0 && value>-1)
+		
+		sentiment="negatif";
+	else if(value<1 && value>0)
+		
+		sentiment="positif";
+	else
+		sentiment="neutre";
 }
 
-	public String getAspect() {
+	public String getaspect() {
 		return Aspect;
 	}
 
-	@Override
-public String toString() {
-	return "Opinionword [ Aspect "+Aspect+" word=" + Oword + ", negation=" + negation + ",   pos=" + pos +" its value"+value+"]";
+	
+   public String toString() {
+	
+	return "Opinionword [ Aspect= "+Aspect+" word=" + word + ", negation=" + negation + ",   pos=" + pos +" its value="+value+ ", sentiment="+sentiment+"]";
+	
 }
 
 	public void setAspect(String aspect) {
 		Aspect = aspect;
 	}
+	public String getSentiment(){
+		return sentiment;
+	}
 }
+
 
